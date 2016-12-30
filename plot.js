9c3c720a956b6d8aadd5d6e4a856b8348f66ed1e
Eugen Keil <eugen.keil@freiheit.com>
2016-12-30 10:25:27 +0100

    A small cleanup of the code.
    - delete unnecessary parts
    - indentation

--- plot.js
+++ plot.js
@@ -12,7 +12,7 @@ basis: [[1,0,0],[0,1,0],[0,0,1]],
 midpoint: [250,250],   // where on the canvas is the shifted origin?
 // angles
 phix: 0,
-phiy: 0, 
+phiy: 0,
 phiz: 0,
 
 // size of canvas
@@ -21,86 +21,90 @@ sizeY: 500,
 
 
 reset: function(){
-this.eye_obs = 100;
-this.zoom = 10;
-this.midpoint = [250,250];
-this.basis = [[1,0,0],[0,1,0],[0,0,1]];
-this.shift = [-10,0,0];
+	this.eye_obs = 100;
+	this.zoom = 10;
+	this.midpoint = [250,250];
+	this.basis = [[1,0,0],[0,1,0],[0,0,1]];
+	this.shift = [-10,0,0];
 },
 
 
 // Main plot function
 plotMain: function(data,color){
-var c=document.getElementById("medium");
-var ctx = c.getContext('2d');
-ctx.clearRect(0,0,this.sizeX,this.sizeY);
+	var c=document.getElementById("medium");
+	var ctx = c.getContext('2d');
+	ctx.clearRect(0,0,this.sizeX,this.sizeY);
 
-ctx.fillStyle="black";
-ctx.fillRect(0,0,this.sizeX,this.sizeY);
-var n,m,k,x,y,d;
+	ctx.fillStyle="black";
+	ctx.fillRect(0,0,this.sizeX,this.sizeY);
+	var n,m,k,x,y,d;
 
-//ctx.globalCompositeOperation = 'destination-over';
+	//ctx.globalCompositeOperation = 'destination-over';
 
-var pdata = [];  //data projected to the plane ?? other surface projections possible?
-var dvec=[];
-
-
-for(n=0;n<data.length;n++){
-for(k=0;k<data[n].length;k++){
-dvec[k]=0;
-for(m=0;m<3;m++) dvec[k]+=this.basis[0][m]*data[n][k][m];
-data[n][k].push(dvec[k]);
-}
-}
-
-data.sort(function(a,b){
-var evalsum1=0;
-var evalsum2=0; 
-for(var k=0;k<b.length;k++) evalsum1+=b[k][b[k].length-1]; 
-for(var k=0;k<a.length;k++) evalsum2+=a[k][a[k].length-1]; 
-return (evalsum1*a.length-evalsum2*b.length);});  // sort according to midpoint
+	var pdata = [];  //data projected to the plane ?? other surface projections possible?
+	var dvec=[];
 
 
-for(n=0;n<data.length;n++){
-for(k=0;k<data[n].length;k++){
-data[n][k].pop();
-}
-}
+	for(n=0;n<data.length;n++) {
+		for(k=0;k<data[n].length;k++) {
+			dvec[k]=0;
+			for(m=0;m<3;m++) {
+				dvec[k]+=this.basis[0][m]*data[n][k][m];
+			}
+			data[n][k].push(dvec[k]);
+		}
+	}
 
-var f;
-for(n=0;n<data.length;n++){
-var pd = this.project(data[n],this.basis,this.eye_obs,this.shift);  // project data-points to surface
-if(pd.length==1){
-ctx.fillStyle = '#333333';
-f=this.eye_obs/(this.eye_obs+pd[0][2]);
-ctx.beginPath();
-ctx.arc(this.midpoint[0]+this.zoom*pd[0][0], this.midpoint[1]-this.zoom*pd[0][1], 10*f, 0, 2 * Math.PI, false);
-ctx.fill();
-} else if(pd.length==2){
-ctx.lineWidth=2;
-ctx.strokeStyle = '#000000';
-//ctx.setLineDash([4, 2]);
-ctx.beginPath();
-ctx.moveTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
-ctx.lineTo(this.midpoint[0]+this.zoom*pd[1][0],this.midpoint[1]-this.zoom*pd[1][1]);
-ctx.closePath();
-ctx.stroke();
-} else if(pd.length > 2){
-ctx.lineWidth=1;
-ctx.fillStyle = color(data[n]);
-//ctx.strokeStyle = ctx.fillStyle;
-//if grid = off dann dies machen..??
-ctx.beginPath();
-ctx.moveTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
-for(k=0;k<pd.length;k++)
-ctx.lineTo(this.midpoint[0]+this.zoom*pd[k][0],this.midpoint[1]-this.zoom*pd[k][1]);
-ctx.lineTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
-ctx.fill();
-ctx.stroke();
-ctx.closePath();
-}
-}
+	data.sort( // sort according to midpoint
+		function(a,b){
+		var evalsum1=0;
+		var evalsum2=0;
+		for(var k=0;k<b.length;k++) evalsum1+=b[k][b[k].length-1];
+		for(var k=0;k<a.length;k++) evalsum2+=a[k][a[k].length-1];
+		return (evalsum1*a.length-evalsum2*b.length);}
+	);
 
+
+	for(n=0;n<data.length;n++){
+		for(k=0;k<data[n].length;k++){
+			data[n][k].pop();
+		}
+	}
+
+	var f;
+	for(n=0;n<data.length;n++){
+		var pd = this.project(data[n],this.basis,this.eye_obs,this.shift);  // project data-points to surface
+		if(pd.length==1){ // draw points
+			ctx.fillStyle = '#333333';
+			f=this.eye_obs/(this.eye_obs+pd[0][2]);
+			ctx.beginPath();
+			ctx.arc(this.midpoint[0]+this.zoom*pd[0][0], this.midpoint[1]-this.zoom*pd[0][1], 10*f, 0, 2 * Math.PI, false);
+			ctx.fill();
+		} else if(pd.length==2) { // draw lines
+			ctx.lineWidth=2;
+			ctx.strokeStyle = '#000000';
+			//ctx.setLineDash([4, 2]);
+			ctx.beginPath();
+			ctx.moveTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
+			ctx.lineTo(this.midpoint[0]+this.zoom*pd[1][0],this.midpoint[1]-this.zoom*pd[1][1]);
+			ctx.closePath();
+			ctx.stroke();
+		} else if(pd.length > 2) { // draw areas, volumes, etc
+			ctx.lineWidth=1;
+			ctx.fillStyle = color(data[n]);
+			//ctx.strokeStyle = ctx.fillStyle;
+			//if grid = off dann dies machen..??
+			ctx.beginPath();
+			ctx.moveTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
+			for(k=0;k<pd.length;k++) {
+				ctx.lineTo(this.midpoint[0]+this.zoom*pd[k][0],this.midpoint[1]-this.zoom*pd[k][1]);
+			}
+			ctx.lineTo(this.midpoint[0]+this.zoom*pd[0][0],this.midpoint[1]-this.zoom*pd[0][1]);
+			ctx.fill();
+			ctx.stroke();
+			ctx.closePath();
+		}
+	}
 },
 
 
@@ -108,26 +112,26 @@ ctx.closePath();
 
 setBasis: function(){
 
-var n,m,k;
+	var n,m,k;
 
-var sinx = Math.sin(this.phix);
-var cosx = Math.cos(this.phix);
-var siny = Math.sin(this.phiy);
-var cosy = Math.cos(this.phiy);
-var sinz = Math.sin(this.phiz);
-var cosz = Math.cos(this.phiz);
+	var sinx = Math.sin(this.phix);
+	var cosx = Math.cos(this.phix);
+	var siny = Math.sin(this.phiy);
+	var cosy = Math.cos(this.phiy);
+	var sinz = Math.sin(this.phiz);
+	var cosz = Math.cos(this.phiz);
 
-var matx = [[1,0,0],[0,cosx,-sinx],[0,sinx,cosx]];
-var maty = [[cosy,0,siny],[0,1,0],[-siny,0,cosy]];
-var matz = [[cosz,-sinz,0],[sinz,cosz,0],[0,0,1]];
+	var matx = [[1,0,0],[0,cosx,-sinx],[0,sinx,cosx]];
+	var maty = [[cosy,0,siny],[0,1,0],[-siny,0,cosy]];
+	var matz = [[cosz,-sinz,0],[sinz,cosz,0],[0,0,1]];
 
 
-// Matrixproducts to get general rotation matrix
-// can one get it with better control on where the end result will look at?
-var prod=this.mat3x3Mult(maty,matx);
-var prod2=this.mat3x3Mult(matz,prod);
+	// Matrixproducts to get general rotation matrix
+	// can one get it with better control on where the end result will look at?
+	var prod=this.mat3x3Mult(maty,matx);
+	var prod2=this.mat3x3Mult(matz,prod);
 
-this.basis = prod2;
+	this.basis = prod2;
 },
 
 
@@ -136,42 +140,42 @@ this.basis = prod2;
 // write a matrix library!!
 
 mat3x3Mult: function (A,B){
-var C=[];
-C[0]=[];
-C[0][0]=A[0][0]*B[0][0]+A[0][1]*B[1][0]+A[0][2]*B[2][0];
-C[0][1]=A[0][0]*B[0][1]+A[0][1]*B[1][1]+A[0][2]*B[2][1];
-C[0][2]=A[0][0]*B[0][2]+A[0][1]*B[1][2]+A[0][2]*B[2][2];
-C[1]=[];
-C[1][0]=A[1][0]*B[0][0]+A[1][1]*B[1][0]+A[1][2]*B[2][0];
-C[1][1]=A[1][0]*B[0][1]+A[1][1]*B[1][1]+A[1][2]*B[2][1];
-C[1][2]=A[1][0]*B[0][2]+A[1][1]*B[1][2]+A[1][2]*B[2][2];
-C[2]=[];
-C[2][0]=A[2][0]*B[0][0]+A[2][1]*B[1][0]+A[2][2]*B[2][0];
-C[2][1]=A[2][0]*B[0][1]+A[2][1]*B[1][1]+A[2][2]*B[2][1];
-C[2][2]=A[2][0]*B[0][2]+A[2][1]*B[1][2]+A[2][2]*B[2][2];
-return C;
+	var C=[];
+	C[0]=[];
+	C[0][0]=A[0][0]*B[0][0]+A[0][1]*B[1][0]+A[0][2]*B[2][0];
+	C[0][1]=A[0][0]*B[0][1]+A[0][1]*B[1][1]+A[0][2]*B[2][1];
+	C[0][2]=A[0][0]*B[0][2]+A[0][1]*B[1][2]+A[0][2]*B[2][2];
+	C[1]=[];
+	C[1][0]=A[1][0]*B[0][0]+A[1][1]*B[1][0]+A[1][2]*B[2][0];
+	C[1][1]=A[1][0]*B[0][1]+A[1][1]*B[1][1]+A[1][2]*B[2][1];
+	C[1][2]=A[1][0]*B[0][2]+A[1][1]*B[1][2]+A[1][2]*B[2][2];
+	C[2]=[];
+	C[2][0]=A[2][0]*B[0][0]+A[2][1]*B[1][0]+A[2][2]*B[2][0];
+	C[2][1]=A[2][0]*B[0][1]+A[2][1]*B[1][1]+A[2][2]*B[2][1];
+	C[2][2]=A[2][0]*B[0][2]+A[2][1]*B[1][2]+A[2][2]*B[2][2];
+	return C;
 },
 
 
 
 project: function(dp,b,e,sh){   // dp=datapoint, b = basis, e = eye-dist, sh = shift
-var xvec = [];
-var yvec = [];
-var fvec=[];
-var hvec = [];
+	var xvec = [];
+	var yvec = [];
+	var fvec=[];
+	var hvec = [];
 
-for(var k=0;k<dp.length;k++){
-	hvec[k] = b[0][0]*(dp[k][0]-sh[0])+b[0][1]*(dp[k][1]-sh[1])+b[0][2]*(dp[k][2]-sh[2]);
-	xvec[k] = b[1][0]*(dp[k][0]-sh[0])+b[1][1]*(dp[k][1]-sh[1])+b[1][2]*(dp[k][2]-sh[2]);
-	yvec[k] = b[2][0]*(dp[k][0]-sh[0])+b[2][1]*(dp[k][1]-sh[1])+b[2][2]*(dp[k][2]-sh[2]);
-	
-	fvec[k]=e/hvec[k];
-	if(hvec[k]<=0.001) return [];  // if any hvec<=0  --> behind observer
-}
+	for(var k=0;k<dp.length;k++){
+		hvec[k] = b[0][0]*(dp[k][0]-sh[0])+b[0][1]*(dp[k][1]-sh[1])+b[0][2]*(dp[k][2]-sh[2]);
+		xvec[k] = b[1][0]*(dp[k][0]-sh[0])+b[1][1]*(dp[k][1]-sh[1])+b[1][2]*(dp[k][2]-sh[2]);
+		yvec[k] = b[2][0]*(dp[k][0]-sh[0])+b[2][1]*(dp[k][1]-sh[1])+b[2][2]*(dp[k][2]-sh[2]);
 
-var projsur = [];
-for(var k=0;k<dp.length;k++) projsur.push([xvec[k]*fvec[k],yvec[k]*fvec[k],hvec[k]]);
-return projsur;
+		fvec[k]=e/hvec[k];
+		if(hvec[k]<=0.001) return [];  // if any hvec<=0  --> behind observer
+	}
+
+	var projsur = [];
+	for(var k=0;k<dp.length;k++) projsur.push([xvec[k]*fvec[k],yvec[k]*fvec[k],hvec[k]]);
+	return projsur;
 
 }
 
--- scripts.js
+++ scripts.js
@@ -9,36 +9,35 @@ var lasty = 0;
 var counter = 0;
 
 function doSomeThing(event){
-x = event.clientX;
-y = event.clientY;
+	x = event.clientX;
+	y = event.clientY;
 
-if(change){
-var c=document.getElementById("medium");
-var ctx = c.getContext('2d');
+	if(change){
+		e3d.phiz+=(x-lastx)/100;
+		e3d.phiy-=(y-lasty)/100;
 
-e3d.phiz+=(x-lastx)/100;
-e3d.phiy-=(y-lasty)/100;
-
-cubePlot();
-}
-lastx = x;
-lasty = y;
+		cubePlot();
+	}
+	lastx = x;
+	lasty = y;
 }
 
 
 window.onkeydown=function(event){
-var charCode = event.keyCode || event.which;
-if(charCode == 65)  moveLeft();  //a
-if(charCode == 87) moveUp();  //  w
-if(charCode == 83) moveDown();  // s
-if(charCode == 68) moveRight();  // d
-if(charCode == 82) moveIn();   // r
-if(charCode == 70) moveOut(); // f
+	var charCode = event.keyCode || event.which;
+	if(charCode == 65)  moveLeft();  //a
+	if(charCode == 87) moveUp();  //  w
+	if(charCode == 83) moveDown();  // s
+	if(charCode == 68) moveRight();  // d
+	if(charCode == 82) moveIn();   // r
+	if(charCode == 70) moveOut(); // f
 
-if(charCode == 13)setCube(); // enter
+	if(charCode == 13)setCube(); // enter
 };
 
-window.onload=function(){cubePlot();};
+window.onload=function(){
+	cubePlot();
+};
 
 
 
@@ -80,30 +79,29 @@ function setCube(){
 
 function cubePlot(){
 
-var n,m;
-var surfaces = [];
+	var n,m;
+	var surfaces = [];
 
-for(n=0;n<cubeVec.length;n++){
-for(m=0;m<cubeVec[n].length;m++){
-	surfaces.push(cubeVec[n][m]);
-}
-}
+	for(n=0;n<cubeVec.length;n++){
+		for(m=0;m<cubeVec[n].length;m++){
+			surfaces.push(cubeVec[n][m]);
+		}
+	}
 
-addCube(surfaces,currentPos);
+	addCube(surfaces,currentPos);
 
+	var basis = e3d.setBasis();
+	for(var k=0;k<3;k++) {
+		e3d.shift[k] = -10*e3d.basis[0][k];
+	}
 
-
-var basis = e3d.setBasis();
-for(var k=0;k<3;k++) e3d.shift[k] = -10*e3d.basis[0][k];
-
-var str = document.getElementById('farbe').value;
-e3d.plotMain(surfaces,function(datapoint){return str;});
-
+	var str = document.getElementById('colourValue').value;
+	e3d.plotMain(surfaces,function(datapoint){return str;});
 }
 
 
 function addCube(surfaces,coord){
-	
+
 	var coord0 = [coord[0],coord[1],coord[2]];
 	var coord1 = [coord[0]+1,coord[1],coord[2]];
 	var coord2 = [coord[0],coord[1]+1,coord[2]];
@@ -112,7 +110,7 @@ function addCube(surfaces,coord){
 	var coord5 = [coord[0]+1,coord[1],coord[2]+1];
 	var coord6 = [coord[0]+1,coord[1]+1,coord[2]];
 	var coord7 = [coord[0]+1,coord[1]+1,coord[2]+1];
-	
+
 	// faces
 	surfaces.push([coord0,coord1,coord6,coord2]);
 	surfaces.push([coord0,coord1,coord5,coord3]);
@@ -120,7 +118,7 @@ function addCube(surfaces,coord){
 	surfaces.push([coord7,coord4,coord3,coord5]);
 	surfaces.push([coord7,coord4,coord2,coord6]);
 	surfaces.push([coord7,coord5,coord1,coord6]);
-	
+
 
 	//edges
 	surfaces.push([coord0,coord1]);
@@ -136,34 +134,4 @@ function addCube(surfaces,coord){
 	surfaces.push([coord7,coord5]);
 	surfaces.push([coord7,coord6]);
 
-}
-
-function f1(n,m,t){
-var lambda=Math.sin(t/10);
-return lambda*Math.sin(Math.PI*n/10)*Math.sin(Math.PI*m/10);
-}
-
-
-function det2x2(mat){
-return mat[0][0]*mat[1][1]-mat[1][0]*mat[0][1];
-}
-
-function vecminus(u,v){
-var w = [];
-w[0]=u[0]-v[0];
-w[1]=u[1]-v[1];
-w[2]=u[2]-v[2];
-return w;
-}
-
-function scalarprod(v,w){
-return v[0]*w[0]+v[1]*w[1]+v[2]*w[2];
-}
-
-function crossprod(u,v){
-var x = [];
-x[0]=det2x2([[u[1],u[2]],[v[1],v[2]]]);
-x[1]=det2x2([[u[2],u[0]],[v[2],v[0]]]);
-x[2]=det2x2([[u[0],u[1]],[v[0],v[1]]]);
-return x;
 }
